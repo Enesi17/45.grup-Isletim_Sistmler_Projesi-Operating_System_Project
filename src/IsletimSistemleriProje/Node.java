76b4f30ac3d99d08f25944454cdda11e26c32572
MYA-65 <104178979+MYA-65@users.noreply.github.com>
2023-12-31 19:40:37 +0300

    Add files via upload

---
+++ src/IsletimSistemleriProje/Node.java
@@ -0,0 +1,25 @@
+package OSProjectProva1;
+
+public class Node {
+		
+		Process process;
+		Node next;
+		Node prev;
+		
+		public Node(Process process) {
+			this.process = process;
+			this.next = null;
+			this.prev = null;
+		}
+		
+		public void delete() {
+			if(prev != null) {
+				prev.next = next;
+			}
+			
+			if(next != null) {
+				next.prev = prev;
+			}
+		}
+}
+
